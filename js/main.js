d955bdd92d092640e3f0653d3514a80fc3279f69
smriti <iamsmritid@gmail.com>
2016-10-18 18:55:22 +0530

    home page created

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// owl carousel js
+
+$(document).ready(function() {
+
+    $("#owl-demo").owlCarousel({
+
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+        items : 4,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+
+    });
+
+});
